29d6d42333e68678ed883ee9d5331db1993cd9cd
yomola <yomola@users.noreply.github.com>
2024-04-21 23:14:29 +0100

    first commit of index.html and main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+//Two ways to output information on javascript
+window.alert("Good morning!");
+document.write("Have a wonderful day!");
+
+// Create two variables
+var Sent1 = "This is the begining of the string";
+var Sent2 = " and this is the end of the string";
+
+//An expression
+var fullSentence = Sent1+Sent2;
